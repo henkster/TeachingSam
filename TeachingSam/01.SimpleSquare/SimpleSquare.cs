eac5ae0c560e3024e8999f259c800d434851c9dd
Steve Henke <steve@henkster.com>
2013-03-27 20:56:00 -0500

    Inital commit

---
+++ TeachingSam/01.SimpleSquare/SimpleSquare.cs
@@ -0,0 +1,34 @@
+﻿using SmallBasicFun;
+
+namespace SimpleSquare
+{
+	public class SimpleSquare
+	{
+		public static void Main()
+		{
+			Tortoise.Show();
+			//  Make the tortoise move as fast as possible  --#6
+
+			//  Do the following 4 times --#5.1
+
+            Tortoise.SetPenColor("blue");
+		    Tortoise.Move(50);
+            Tortoise.Turn(90);
+
+            Tortoise.SetPenColor("blue");
+            Tortoise.Move(50);
+            Tortoise.Turn(90);
+
+            Tortoise.SetPenColor("blue");
+            Tortoise.Move(50);
+            Tortoise.Turn(90);
+
+            Tortoise.SetPenColor("blue");
+            Tortoise.Move(50);
+            Tortoise.Turn(90);
+
+            //  Repeat --#5.2
+
+		}
+	}
+}
